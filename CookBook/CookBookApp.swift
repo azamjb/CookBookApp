8c005d384b439705ee55e4f5fc0dd15c57d2655c
Azam Jawad Butt <azamjb123@gmail.com>
2022-11-02 16:21:18 -0400

    Initial Commit

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Azam Jawad on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
